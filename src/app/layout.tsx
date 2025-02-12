5b2db1a6bf070fa9c427a39d211d1f25ac5a947b
mwibutsa-koin <>
2025-02-12 23:31:10 +0200

    Improve responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased light`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased light pl-14 md:pl-0`}
       >
         <AccountProvider>{children}</AccountProvider>
         <ToastContainer />
